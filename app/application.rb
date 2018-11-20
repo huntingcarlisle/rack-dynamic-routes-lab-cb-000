cfa4f0ab0c3e9b80a6d5917d8171a077048f87a3
Hunter Carlisle <huntingcarlisle@gmail.com>
2018-11-20 05:48:39 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -19,21 +19,21 @@ class Application
     resp.finish
   end
 
-  def handle_search(search_term)
-    if @@items.include?(search_term)
-      return "#{search_term} is one of our items"
-    else
-      return "Couldn't find #{search_term}"
-    end
-  end
+  # def handle_search(search_term)
+  #   if @@items.include?(search_term)
+  #     return "#{search_term} is one of our items"
+  #   else
+  #     return "Couldn't find #{search_term}"
+  #   end
+  # end
   
-  def handle_add(search_term)
-    if @@items.include?(search_term)
-      @@cart << search_term
-      return "added #{search_term}"
-    else
-      return "We don't have that item"
-    end
-  end
+  # def handle_add(search_term)
+  #   if @@items.include?(search_term)
+  #     @@cart << search_term
+  #     return "added #{search_term}"
+  #   else
+  #     return "We don't have that item"
+  #   end
+  # end
   
 end
