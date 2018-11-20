41350dcd3f55bb96b73e1c9a75beca8fe533fad7
Hunter Carlisle <huntingcarlisle@gmail.com>
2018-11-20 05:57:50 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -9,8 +9,8 @@ class Application
 
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
-      if @@items.include?(item_name)
-        item = @@items.find{|i| i.name == item_name}
+      item = @@items.find{|i| i.name == item_name}
+      if item
         resp.write item.price
       else
         resp.write "Item not found"
